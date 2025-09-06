56e4fa5e4d01137cf39726e4cbf41005a8ee25a4
TheCodeLamp <HIDDEN>
2025-09-06 14:08:48 +0200

    Add megasync

--- build_files/install-stage-megasync.sh
+++ build_files/install-stage-megasync.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 set -ouex pipefail
+
+source /etc/os-release
+
+FILE="/tmp/megasync.rpm"
+curl -L -o $FILE "https://mega.nz/linux/repo/Fedora_$VERSION_ID/x86_64/megasync-Fedora_$VERSION_ID.x86_64.rpm"
+dnf install --assumeyes $FILE
+rm $FILE
+
+FILE="/tmp/megasync-dolphin.rpm"
+curl -L -o $FILE "https://mega.nz/linux/repo/Fedora_$VERSION_ID/x86_64/dolphin-megasync-Fedora_$VERSION_ID.x86_64.rpm"
+dnf install --assumeyes $FILE
+rm $FILE
