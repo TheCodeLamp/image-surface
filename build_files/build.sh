7e1e8cae3e817d096eeee790c8002044babbe20c
TheCodeLamp <12064217+TheCodeLamp@users.noreply.github.com>
2025-09-12 14:20:53 +0200

    Use relative paths

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,65 +2,35 @@
 
 set -oue pipefail
 
+cd /ctx
+
 # Update
 echo "::group:: Update"
-/ctx/main-repo/build_files/update-stage.sh
+./main-repo/build_files/update-stage.sh
 echo "::endgroup::"
 
 # Remove packages
 echo "::group:: Remove packages"
-/ctx/main-repo/build_files/remove-packages.sh
+./main-repo/build_files/remove-packages.sh
 echo "::endgroup::"
-
-# Install Packages
 
 # Install Kernel
 echo "::group:: Install Surface Kernel"
-/ctx/install-stage-surface-kernel.sh
+./install-stage-surface-kernel.sh
 echo "::endgroup::"
 
-## Install Mullvad
-echo "::group:: Install Mullvad"
-/ctx/main-repo/build_files/install-stage-mullvad.sh
-echo "::endgroup::"
-
-## Install Megasync
-echo "::group:: Install Megasync"
-/ctx/main-repo/build_files/install-stage-megasync.sh
-echo "::endgroup::"
-
-## Install Nushell
-echo "::group:: Install Nushell"
-/ctx/main-repo/build_files/install-stage-nushell.sh
-echo "::endgroup::"
-
-## Install LibreWolf
-echo "::group:: Install LibreWolf"
-/ctx/main-repo/build_files/install-stage-librewolf.sh
-echo "::endgroup::"
-
-## Install FirefoxPWA
-echo "::group:: Install FirefoxPWA"
-/ctx/main-repo/build_files/install-stage-PWAsForFirefox.sh
-echo "::endgroup::"
-
-## Install Misc
-echo "::group:: Install Misc"
-/ctx/main-repo/build_files/install-stage-misc.sh
-echo "::endgroup::"
-
-## Install Fonts
-echo "::group:: Install Fonts"
-/ctx/main-repo/build_files/install-stage-fonts.sh
-echo "::endgroup::"
+# Install Main Image
+cd /ctx/main-repo/build_files
+./install-stage.sh
+cd /ctx
 
 # Generate initramfs
 echo "::group:: Generate initramfs"
-/ctx/generate-initramfs.sh
+./generate-initramfs.sh
 echo "::endgroup::"
 
 # Cleanup
 echo "::group:: Cleanup"
-/ctx/main-repo/build_files/clean.sh
+./main-repo/build_files/clean.sh
 echo "::endgroup::"
 
